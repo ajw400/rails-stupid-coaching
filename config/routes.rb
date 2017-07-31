fd27ce6c82264e5b442d1cfec559ade5f1d8f685
Andrew Wright <prestoassai@hotmail.com>
2017-07-31 11:55:35 +0200

    exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'coaching_controller/answer'
+
+  get 'coaching_controller/ask'
+
+
+  get 'answer', to: 'coaching_controller#answer'
+  get 'ask', to: 'coaching_controller#ask'
+
 end
